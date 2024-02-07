40e852231eb91fac105bb688bd85b41eb0a77f38
sakshishah5 <118061433+sakshishah5@users.noreply.github.com>
2024-02-07 17:14:24 +0000

    DT Conversion

---
+++ Day_02/datatype_conversion.js
@@ -0,0 +1,26 @@
+let score = "33"
+console.log(typeof score)
+
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber)
+console.log(scoreInNumber)
+
+//"33" --> 33
+//"33abc" --> NaN
+//true --> 1; false --> 0
+
+let isLoggedIn = 1
+console.log(typeof isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); //true;
+
+// 1 --> true; 0 --> false
+// "" --> false
+// "Sakshi" -->true
+
+let a = 33
+
+let stringA = String(a)
+console.log(typeof stringA); //string
+console.log(stringA); //33
