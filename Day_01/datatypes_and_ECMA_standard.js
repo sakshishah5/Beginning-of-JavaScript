b55942eadf1d18b142d9f4b4e96d63b556443caf
sakshishah5 <118061433+sakshishah5@users.noreply.github.com>
2024-02-07 16:48:42 +0000

    Data-Types

---
+++ Day_01/datatypes_and_ECMA_standard.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version, well it is not compolsory but it's a good practice to do so
+
+// alert("Hello") //we are using nodeJs, not browser
+
+console.log(3+3); console.log("Sakshi")//not good for code readability
+
+
+//Data-Types: 
+let name = "Sakshi" //String Datatype
+let age = 20 //Number DataType
+let isRight = true //boolean
+// null => standalone value
+// undefined
+// Symbol => unique
+
+console.log(typeof undefined) //gives output as undefined
+console.log(typeof null) //gives output as object
